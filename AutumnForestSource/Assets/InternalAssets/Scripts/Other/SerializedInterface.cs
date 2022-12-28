0bb1c26715385c3b19e142f359f37a1c72e550ea
YUJECK <76834558+YUJECK@users.noreply.github.com>
2022-12-29 00:05:36 +0300

    У контрибьютора шиза

---
+++ AutumnForestSource/Assets/InternalAssets/Scripts/Other/SerializedInterface.cs
@@ -0,0 +1,25 @@
+﻿using AutumnForest.Editor;
+using UnityEngine;
+
+namespace AutumnForest
+{
+    [System.Serializable]
+    public sealed class SerializedInterface<T> where T : class
+    {
+        private T test;
+
+        [SerializeField, Interface()] private Object interfaceObject;
+
+        private T _interface;
+        public T Interface
+        {
+            get
+            {
+                if (_interface == null)
+                    _interface = _interface as T;
+
+                return _interface;
+            }
+        }
+    }
+}

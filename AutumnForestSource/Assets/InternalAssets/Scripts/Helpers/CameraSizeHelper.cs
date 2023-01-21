6cd5eae10106b5bc08bcc94051e4019f7b5b5a70
YUJECK <76834558+YUJECK@users.noreply.github.com>
2023-01-21 19:50:29 +0300

    Добавил НИП-ов на карту

--- AutumnForestSource/Assets/InternalAssets/Scripts/Helpers/CameraSizeHelper.cs
+++
@@ -1,9 +0,0 @@
-namespace AutumnForest.Helpers
-{
-    public struct CameraSizeHelper
-    {
-        public const int Default = 3;
-        public const int BossFight = 7;
-        public const int Slingshot = 4;
-    }
-}

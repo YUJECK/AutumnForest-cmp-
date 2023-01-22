394806d8f4cdf9d7c3795fe3f9b20b4f025eeb69
YUJECK <76834558+YUJECK@users.noreply.github.com>
2023-01-22 19:09:50 +0300

    Доработал рогатку
    +отключение рогатки при выходе из триггера
    +аудио еффект рогатке

---
+++ AutumnForestSource/Assets/InternalAssets/Scripts/BossFight/Slingshot/SlingshotAudioEffect.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace AutumnForest.BossFight.Slingshot
+{
+    public class SlingshotAudioEffect : MonoBehaviour
+    {
+        [SerializeField] private AudioSource shotAudioEffect;
+        private Slingshot slingshot;
+
+        //может это все таки лучше было бы интегрировать в сам скрипт рогатки /:
+
+        private void Awake() => slingshot = GetComponent<Slingshot>();
+        private void OnEnable() => slingshot.OnShoot += shotAudioEffect.Play;
+        private void OnDisable() => slingshot.OnShoot -= shotAudioEffect.Play;
+    }
+}
--- AutumnForestSource/Assets/InternalAssets/Scripts/BossFight/Slingshot/SlingshotInteractive.cs
+++ AutumnForestSource/Assets/InternalAssets/Scripts/BossFight/Slingshot/SlingshotInteractive.cs
@@ -1,22 +1,34 @@
 ﻿using AutumnForest.Helpers;
 using AutumnForest.Other;
 using Cinemachine;
-using System;
 using UnityEngine;
 
 namespace AutumnForest.BossFight.Slingshot
 {
     [RequireComponent(typeof(Slingshot))]
+    [RequireComponent(typeof(Collider2D))]
     public sealed class SlingshotInteractive : MonoBehaviour, IInteractive
     {
         [SerializeField] private Sprite slingshotCursor;
         [SerializeField] private Sprite cursorDefault;
 
+        private bool currentlyZoomed = false;
         private Slingshot slingshot;
 
         private void Awake() => slingshot = GetComponent<Slingshot>();
         private void OnEnable() => slingshot.OnShoot += Distance;
         private void OnDisable() => slingshot.OnShoot -= Distance;
+        
+        // мне если честно не особо нравится такое решение отключения рогатки,
+        // но я хз что еще придумать(мне лень) 
+        private void OnTriggerExit2D(Collider2D collision)
+        {
+            if (collision.CompareTag(TagHelper.PlayerTag))
+            {
+                slingshot.DisableSlingshot();
+                Distance();
+            }
+        }
 
         public void Interact()
         {
@@ -26,17 +38,25 @@ namespace AutumnForest.BossFight.Slingshot
 
         private void Zoom()
         {
-            GlobalServiceLocator.GetService<CinemachineBrain>().ActiveVirtualCamera.VirtualCameraGameObject.SetActive(false);
-            GlobalServiceLocator.GetService<VirtualCameraHelper>().SlingshotCamera.gameObject.SetActive(true);
+            if(!currentlyZoomed)
+            {
+                GlobalServiceLocator.GetService<CinemachineBrain>().ActiveVirtualCamera.VirtualCameraGameObject.SetActive(false);
+                GlobalServiceLocator.GetService<VirtualCameraHelper>().SlingshotCamera.gameObject.SetActive(true);
             
-            FindObjectOfType<Cursor>().SetCursorIcon(slingshotCursor);
+                FindObjectOfType<Cursor>().SetCursorIcon(slingshotCursor);
+                currentlyZoomed = true;
+            }
         }
         private void Distance()
         {
-            GlobalServiceLocator.GetService<CinemachineBrain>().ActiveVirtualCamera.VirtualCameraGameObject.SetActive(false);
-            GlobalServiceLocator.GetService<VirtualCameraHelper>().BossfightCamera.gameObject.SetActive(true);
+            if(currentlyZoomed)
+            {
+                GlobalServiceLocator.GetService<CinemachineBrain>().ActiveVirtualCamera.VirtualCameraGameObject.SetActive(false);
+                GlobalServiceLocator.GetService<VirtualCameraHelper>().BossfightCamera.gameObject.SetActive(true);
 
-            FindObjectOfType<Cursor>().SetCursorIcon(cursorDefault);
+                FindObjectOfType<Cursor>().SetCursorIcon(cursorDefault);
+                currentlyZoomed = false;
+            }
         }
     }
 }

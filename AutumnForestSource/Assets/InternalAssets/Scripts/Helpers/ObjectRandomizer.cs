79ba88ebc202fcc3faddd7e5278188f642965d1e
YUJECK <76834558+YUJECK@users.noreply.github.com>
2023-01-31 22:25:38 +0300

    Добавил ObjectRandomizer

---
+++ AutumnForestSource/Assets/InternalAssets/Scripts/Helpers/ObjectRandomizer.cs
@@ -0,0 +1,7 @@
+﻿namespace AutumnForest.Helpers
+{
+    public sealed class ObjectRandomizer
+    {
+        public static T GetRandom<T>(params T[] objects) => objects[UnityEngine.Random.Range(0, objects.Length)];
+    }
+}
